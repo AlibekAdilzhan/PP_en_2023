9c765608f945cbc4dab5b13a1430790bef8ece06
AlibekAdilzhan <alibek.adilzhanov@gmail.com>
2023-11-06 12:28:20 +0600

    week 9

---
+++ week_9/cw1.py
@@ -0,0 +1,10 @@
+list_1 = [1, 2, -8, 9, 10]
+
+# list_2 = []
+# for i in list_1:
+#     list_2.append(i**2)
+# print(list_2)
+
+# [x for x in container]
+list_2 = [i**2 for i in list_1]
+print(list_2)
---
+++ week_9/cw3.py
@@ -0,0 +1,7 @@
+# 1st task: you have a list [1, -1, 5, 8, 1] create a list with cubes
+# 2nd task: you have 2 lists l1, l2 of the same size (len). Create a new list with 
+# the sums of elements in those lists
+# ex1: 
+# l1 = [1, 2, 3]
+# l2 = [-1, 0, 5]
+# l_sum = [0, 2, 8]
